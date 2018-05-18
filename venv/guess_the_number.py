40302c111ecb07d812cdefaeeb49faac4a3980dd
ajnai01 <akshnayi802@gmail.com>
2018-05-18 08:14:39 -0400

    my final submission

--- venv/guess_the_number.py
+++ venv/guess_the_number.py
@@ -6,32 +6,21 @@ print ('hello! what is your name?')
 my_name = input()
 
 number = random.randint(1,100)
-print ('well,'+ my_name+',i am thinking of a number between 1 and 100.')
+print ('well,'+ my_name +'i am thinking of a number between 1 and 100.')
 
-while guess_taken <6:
-    print('take a guess.')
-    guess = input()
-    guess = int(7)
+user_guess=0
 
-    guess_taken = guess_taken + 1
+while user_guess != number:
+    user_guess = int(input("Take guess: "))
 
-    if guess<number:
-      print('you guess is too low.')
+    if user_guess<number:
+      print( my_name  +  'your guess is too low.')
 
-    if guess>9:
-      print('your guess is too high.')
+    elif user_guess>number:
+      print( my_name  +  'your guess is too high.')
 
-    if guess==number:
+    elif user_guess==number:
+        print( my_name +  "you figured it out!" + 'congrats!!')
         break
-
-if guess==number:
-    guess_taken=str(guess_taken)
-    print('good job,'+my_name+'! you guessed my number in'+number)
-
-if guess!=number:
-    number=str(number)
-    print('nope. the number i was thinking of was '+number)
-
-
 
 
